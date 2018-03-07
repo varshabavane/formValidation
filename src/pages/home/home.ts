7a7a93756e9b5bed6070853a320ee1766f782d1e
varsha bavane <varshab13bavane@gmail.com>
2018-03-07 10:55:02 +0530

    two inputs are given and onclick event function is creted

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  fullName = {
+  firstName:null,
+  lastName:null
+  }
+
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  getFullName(){
+    if(this.fullName.firstName!=='' && this.fullName.lastName!==''){
+     alert(this.fullName.firstName +' ' + this.fullName.lastName)
+
+
+    }else{
+      alert("please fill the detail apropriately")
+    }
+
+  
+ 
   }
 
 }
